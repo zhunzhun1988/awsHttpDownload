97335c1cb3f0a7d709ac6de14f760da1e05a8382
patrickxie <635457511@qq.com>
2017-06-09 11:18:32 +0800

    first commit

---
+++ httpdown.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"sort"
+	"strings"
+
+	"log"
+	"net/http"
+
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
+	"github.com/spf13/pflag"
+)
+
+type awsHandler struct {
+	AccessKey  string
+	SecretKey  string
+	S3Endpoint string
+}
+
+func getBucketNameFromPath(p string) string {
+	if strings.HasPrefix(p, "/") {
+		parts := strings.Split(p, "/")
+		if len(parts) >= 2 {
+			return parts[1]
+		}
+	}
+	return ""
+}
+
+func getBucketPathFromPath(p string) string {
+	if strings.HasPrefix(p, "/") {
+		parts := strings.Split(p, "/")
+		if len(parts) > 2 {
+			ret := parts[2]
+			for _, part := range parts[3:] {
+				ret += "/" + part
+			}
+			return ret
+		}
+	}
+	return ""
+}
+func (awsh *awsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s request %s\n", r.RemoteAddr, r.URL.Path)
+	var auth aws.Auth
+	auth.AccessKey = awsh.AccessKey
+	auth.SecretKey = awsh.SecretKey
+
+	var region aws.Region
+	region.S3Endpoint = awsh.S3Endpoint
+
+	client := s3.New(auth, region)
+	resp, err := client.ListBuckets()
+
+	if err != nil {
+		log.Fatal(err)
+		http.Error(w, fmt.Sprintf("ListBuckets err %v", err), http.StatusNotFound)
+		return
+	}
+
+	if len(resp.Buckets) == 0 {
+		http.Error(w, fmt.Sprintf("len(Buckets)==0"), http.StatusNotFound)
+		return
+	}
+
+	bucketName, bucketPath := getBucketNameFromPath(r.URL.Path), getBucketPathFromPath(r.URL.Path)
+
+	var bucket *s3.Bucket
+	if bucketName == "" {
+		for _, b := range resp.Buckets {
+			w.Write([]byte(fmt.Sprintf("%s\n", b.Name)))
+		}
+		return
+	} else {
+		for i, b := range resp.Buckets {
+			if b.Name == bucketName {
+				bucket = &resp.Buckets[i]
+				break
+			}
+		}
+		if bucket == nil {
+			http.Error(w, fmt.Sprintf("bucket %s is not found", bucketName), http.StatusNotFound)
+			return
+		}
+	}
+
+	bc, err := bucket.GetBucketContents()
+	if err == nil {
+		if bucketPath == "" {
+			paths := make([]string, 0, len(*bc))
+			for s, _ := range *bc {
+				paths = append(paths, s)
+			}
+			sort.Strings(paths)
+			for i, _ := range paths {
+				w.Write([]byte(fmt.Sprintf("%s\n", paths[len(paths)-i-1])))
+			}
+			return
+		} else {
+			var findPath bool
+			var size int64
+			for s, key := range *bc {
+				if path.Clean(s) == path.Clean(bucketPath) {
+					findPath = true
+					size = key.Size
+					break
+				}
+			}
+			if findPath == false {
+				http.Error(w, fmt.Sprintf("cannot find path:%s in bucket:%s", bucketPath, bucketName), http.StatusNotFound)
+				return
+			}
+			connectRead, err := bucket.GetReader(path.Clean(bucketPath))
+			if err != nil {
+				http.Error(w, fmt.Sprintf("get connectRead err:%v", err), http.StatusNotFound)
+				return
+			}
+			io.CopyN(w, connectRead, size)
+		}
+	} else {
+		http.Error(w, fmt.Sprintf("GetBucketContents err=%v", err), http.StatusNotFound)
+		return
+	}
+
+}
+
+func main() {
+	var accessKey, secretKey, s3Endpoint, port string
+	fs := pflag.CommandLine
+	fs.StringVar(&accessKey, "accesskey", "", "aws accesskey")
+	fs.StringVar(&secretKey, "secretkey", "", "aws secretkey")
+	fs.StringVar(&s3Endpoint, "s3endpoint", "", "aws region s3endpoint")
+	fs.StringVar(&port, "port", "", "httpserver listen port")
+	pflag.Parse()
+	awsh := &awsHandler{
+		AccessKey:  accessKey,  //"LYLMTYEH6YC5VIJ04USX",
+		SecretKey:  secretKey,  // "tPiENzJDy88xawCDgYvOY84HWHpHWfoa56XZRm3X",
+		S3Endpoint: s3Endpoint, //"http://10.19.248.200:30150",
+	}
+	if accessKey == "" {
+		log.Printf("accessKey should not be empty")
+		os.Exit(-1)
+	}
+	if secretKey == "" {
+		log.Printf("secretKey should not be empty")
+		os.Exit(-1)
+	}
+	if s3Endpoint == "" {
+		log.Printf("s3Endpoint should not be empty")
+		os.Exit(-1)
+	}
+	if port == "" {
+		log.Printf("port should not be empty")
+		os.Exit(-1)
+	}
+	http.Handle("/", awsh)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		os.Exit(-1)
+	}
+}
